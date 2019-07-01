34d03ecd394ac98a1bb73bc37f435abcf35f4430
1163710126 <2303128509@qq.com>
2019-07-01 22:51:14 +0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mystring = Console.ReadLine();
+            mystring = mystring.Replace("no", "yes");
+        
+            Console.WriteLine("{0}", mystring);
+            Console.ReadKey();
+        }
+      
+
+    }
+}
